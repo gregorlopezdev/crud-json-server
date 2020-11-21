fd949ce94824b37854b11715ee367c3162672c08
Gregory J Lopez <gregorlopez.dev@gmail.com>
2020-11-21 15:32:09 -0300

    feat: se extrajo toda la logica al componente App,

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import CssBaseline from '@material-ui/core/CssBaseline'
-import UMenubar from './components/utils/UMenubar.component'
-import Home from './components/common/Home.component.jsx'
-import Course from './components/common/Course.component.jsx'
-import Notfound from './components/common/Notfound.component.jsx'
+import App from './App.component'
 
-ReactDOM.render(
-  <Router>
-    <CssBaseline />
-    <UMenubar />
-    <Switch>
-      <Route path='/' exact={true}>
-        <Home />
-      </Route>
-      <Route path='/courses/:id'>
-        <Course />
-      </Route>
-      <Route path='*'>
-        <Notfound />
-      </Route>
-    </Switch>
-  </Router>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
