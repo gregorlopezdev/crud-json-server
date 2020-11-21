6aa580fb348b847d576a9269daaa9e5dc520b4de
Gregory J Lopez <gregorlopez.dev@gmail.com>
2020-11-21 18:37:13 -0300

    feat: se maqueto el componente Notfound.

--- src/components/common/Notfound.component.jsx
+++ src/components/common/Notfound.component.jsx
@@ -1,9 +1,24 @@
+import { Alert, AlertTitle } from '@material-ui/lab'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+
 const Notfound = () => {
+  const classes = useStyles()
+
   return (
-    <>
-      <p>Ruta no encontrada</p>
-    </>
+    <Alert className={classes.Notfound} severity='error'>
+      <AlertTitle>Error 404</AlertTitle>
+      Page not found - <strong>try again with another route!</strong>
+    </Alert>
   )
 }
 
 export default Notfound
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    Notfound: {
+      maxWidth: '500px',
+      margin: '0 auto'
+    }
+  })
+)
