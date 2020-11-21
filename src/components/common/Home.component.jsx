e05be152e631a3996cc76d9fd32b882afec38c40
Gregory J Lopez <gregorlopez.dev@gmail.com>
2020-11-21 15:27:57 -0300

    feat: se le agrego una nueva prop al momento de usar UCard para habilitar el redirect al hacer click sobre ella.

--- src/components/common/Home.component.jsx
+++ src/components/common/Home.component.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import useApi from '../../hooks/useApi.hook'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
 
-import UMenubar from '../utils/UMenubar.component'
 import UCard from '../utils/UCard.component'
 
 const Home = () => {
@@ -13,22 +12,19 @@ const Home = () => {
   useEffect(() => {
     apiHook.getAllCourses().then((res) => {
       setCourses(res.data)
-      console.log(res.data)
+      // console.log(res.data)
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <>
-      <UMenubar />
-      <article className={classes.grid}>
-        {courses.map((course, key) => (
-          <div key={key} className={classes.child}>
-            <UCard course={course} />
-          </div>
-        ))}
-      </article>
-    </>
+    <article className={classes.grid}>
+      {courses.map((course, key) => (
+        <div key={key}>
+          <UCard course={course} disabled={false} />
+        </div>
+      ))}
+    </article>
   )
 }
 
@@ -37,12 +33,11 @@ export default Home
 const useStyles = makeStyles((theme) =>
   createStyles({
     grid: {
-      margin: `${theme.spacing(3)}px auto`,
+      // margin: `${theme.spacing(3)}px auto`,
       display: 'grid',
-      gridTemplateColumns: '30% 30%',
+      gridTemplateColumns: '30% 30% 30%',
       gridGap: theme.spacing(3),
       justifyContent: 'center'
-    },
-    child: {}
+    }
   })
 )
