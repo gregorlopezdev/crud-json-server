ff381ec60012bda5c798e28430eb9f139009378d
Gregory J Lopez <gregorlopez.dev@gmail.com>
2020-11-21 15:27:04 -0300

    feat: se actualizo el componente UCard, se le agrego un "chip" para visualizar si el curso esta disponible y se le agrego una nueva prop "disable" que activa o desactiva el redirect al hacer click sobre el componente UCard.

--- src/components/utils/UCard.component.jsx
+++ src/components/utils/UCard.component.jsx
@@ -5,34 +5,48 @@ import {
   CardContent,
   CardMedia,
   CardActions,
-  CardActionArea
+  CardActionArea,
+  Chip
 } from '@material-ui/core'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
-// import { Search, Menu } from '@material-ui/icons'
+import { Block, Check } from '@material-ui/icons'
 
-const UCard = ({ course }) => {
+import { useHistory } from 'react-router-dom'
+
+const UCard = ({ course, disabled }) => {
   const classes = useStyles()
+  const history = useHistory()
+
+  const onNavigate = (id) => {
+    history.push(`/courses/${id}`)
+  }
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
+      <CardActionArea onClick={() => disabled || onNavigate(course.id)}>
         <CardMedia
           className={classes.media}
-          image={course.url_poster}
+          image={course.url_poster_medium || 'https://via.placeholder.com/500'}
           title={course.name}
         />
         <CardContent>
           <Typography gutterBottom variant='h5' component='h2'>
-            {course.name}
+            {course.name || 'Untitle'}
           </Typography>
           <Typography variant='body2' color='textSecondary' component='p'>
             {course.description}
           </Typography>
+          <Chip
+            className={classes.u_chip}
+            icon={course.available ? <Check /> : <Block />}
+            label={course.available ? 'Available' : 'Unavailable'}
+            color={course.available ? 'primary' : 'secondary'}
+          />
         </CardContent>
       </CardActionArea>
-      <CardActions className={classes.actions}>
-        <Button size='small' color='primary'>
-          Precio: {course.price}$
+      <CardActions className={classes.u_actions}>
+        <Button size='small' color='primary' disableRipple={true}>
+          Price: ${course.price}
         </Button>
       </CardActions>
     </Card>
@@ -47,10 +61,14 @@ const useStyles = makeStyles((theme) =>
       maxWidth: '100%'
     },
     media: {
-      height: 160
+      height: 220
     },
-    actions: {
+    u_actions: {
       justifyContent: 'center'
+    },
+    u_chip: {
+      width: '100%',
+      marginTop: theme.spacing(2)
     }
   })
 )
